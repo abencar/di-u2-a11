7fbfc9077c3bc5dd2bbadc88b190a9d9f6cc2cbf
Álvaro Benítez Carmona <76166064+abencar@users.noreply.github.com>
2024-11-22 14:14:28 +0100

    Hecho

--- src/app/Ejercicio4.js
+++ src/app/Ejercicio4.js
@@ -11,31 +11,35 @@ const initialTodos = [
 ];
 
 export default function TaskApp() {
-  const [todos, setTodos] = useState(
-    initialTodos
-  );
+  const [todos, setTodos] = useImmer(initialTodos);
 
   function handleAddTodo(title) {
-    todos.push({
-      id: nextId++,
-      title: title,
-      done: false
+    setTodos(draft => {
+      draft.push({
+        id: nextId++,
+        title: title,
+        done: false
+      });
     });
   }
 
   function handleChangeTodo(nextTodo) {
-    const todo = todos.find(t =>
-      t.id === nextTodo.id
-    );
-    todo.title = nextTodo.title;
-    todo.done = nextTodo.done;
+    setTodos(draft => {
+      const todo = draft.find(t => t.id === nextTodo.id);
+      if (todo) {
+        todo.title = nextTodo.title;
+        todo.done = nextTodo.done;
+      }
+    });
   }
 
   function handleDeleteTodo(todoId) {
-    const index = todos.findIndex(t =>
-      t.id === todoId
-    );
-    todos.splice(index, 1);
+    setTodos(draft => {
+      const index = draft.findIndex(t => t.id === todoId);
+      if (index !== -1) {
+        draft.splice(index, 1);
+      }
+    });
   }
 
   return (
